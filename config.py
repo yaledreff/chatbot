0ec016ee72bafef29b1aebadf2a1a2cfff0e46aa
Yann Le Dreff <yaledreff@gmail.com>
2022-09-07 17:20:43 +0200

    Set APP ID and PASSWORD

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = "acaf63a6-d3c6-49cc-b943-cab77be92e6c"
+    APP_PASSWORD = "53df7d73-508a-4308-9d42-eff3444371b6"
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
