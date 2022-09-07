c10ffe6346107b9e3fb58d47475dba7569574a9f
Yann Le Dreff <yaledreff@gmail.com>
2022-09-07 15:56:00 +0200

    Create init_fun for aiohttp run

--- app.py
+++ app.py
@@ -65,10 +65,13 @@ async def messages(req: Request) -> Response:
         return json_response(data=response.body, status=response.status)
     return Response(status=HTTPStatus.OK)
 
-APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+def init_func(argv):
+  APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
+  APP.router.add_post("/api/messages", messages)
+  return APP
 
 if __name__ == "__main__":
+    APP = init_func(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
