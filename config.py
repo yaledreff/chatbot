3f4791e258daf1501dff0d23938c420c816598aa
Yann Le Dreff <yaledreff@gmail.com>
2022-09-08 13:27:31 +0200

    scfqsfs

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "acaf63a6-d3c6-49cc-b943-cab77be92e6c")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ui4ce7BPF7s.LXNVy8JDGiI04spjVSXsSwGKS6yQd3qNAwnTtdbR-ic")
+    APP_ID = os.environ.get("MicrosoftAppId", "bd5930ee-1a86-48d8-b694-b4dbd6defbd9")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "GsK8Q~Sv2Z~IJ8Y1WaXjGLmLZQcAZnsgDCf7BcHj")
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
